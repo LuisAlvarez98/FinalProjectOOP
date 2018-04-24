0031a8c0441a6317b9b6a23a40d71b0a63daaf08
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2018-04-23 21:46:19 -0600

    added basic menu

--- Fecha.h
+++ Fecha.h
@@ -1,4 +1,23 @@
 #pragma once
 class Fecha {
+	private:
+		int dd;
+		int mm;
+		int aa;
+	public:
+		Fecha();
+		Fecha(int dd, int mm, int aa);
+		friend Fecha operator+(Fecha f1, Fecha f2) {
+			Fecha f3;
+			f3.setDia(f1.getDia() + f2.getDia());
+			return f3;
+		}
+		void setFecha(int dd, int mm, int aa);
+		void setDia(int dd) {
+			this->dd = dd;
+		}
+		int getDia(){
+			return dd;
+		}
 
 };
