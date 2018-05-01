bdb2bff9c9aed7ce0c49588a6a41977e0962f6d6
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2018-05-01 12:43:17 -0600

    reserva loading works

--- Reserva.h
+++ Reserva.h
@@ -6,6 +6,20 @@ class Reserva {
 		int idCliente;
 		Fecha fechaReservacion;
 	public:
+		Reserva() {
+			idMaterial = 0;
+			idCliente = 0;
+			fechaReservacion.setFecha(0, 0, 0);
+		}
+		Reserva(int idMaterial, int idCliente, int dd, int mm, int aa) {
+			this->idMaterial = idMaterial;
+			this->idCliente = idCliente;
+			fechaReservacion.setFecha(dd, mm, aa);
+		}
 		Fecha calculaFechaFinReserva(int cantDias);
-
+		void muestra() {
+			cout << "idMaterial: "  << idMaterial << "\n";
+			cout <<"idCliente: " << idCliente << "\n";
+			cout << "Fecha: " <<fechaReservacion.getDia() << " " << fechaReservacion.getMes() << " " << fechaReservacion.getYear() << "\n";
+ 		}
 };
